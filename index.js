af88c02af3587a448adcff294ca77601e6fc6f6e
AhmidouA <ahmedakkioui75@gmail.com>
2023-03-31 00:35:52 +0200

    random cloud, time and head

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const hole = document.querySelectorAll('.hole')
+const clouds = document.querySelectorAll('.hole')
 const scoreBoard = document.querySelector('.score')
 const heads = document.querySelectorAll('.head');
 
 
+let lastCloud;
+
 let timeUp = false; // le jeu n'est pas fini = false
 let score = 0;
+
+
+function randomTime (min, max){
+    return Math.round(Math.random() * (max - min) + min ); 
+}
+
+function randomCloud(clouds){
+    const indexCloud = math.floor(Math.random() * clouds.length);
+    const cloudSelect = clouds[indexCloud];
+    if (cloudSelect === lastCloud){
+        return randomCloud(clouds) // la recursion
+    }
+    return cloudSelect;
+}
+
+function showHead() {
+    const time = randomTime(600, 1000); // time en milliseconde
+    const cloud = randomCloud(clouds)
+}

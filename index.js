a57ce65b3e48c07aafb7f01d9b351333a8c75545
AhmidouA <ahmedakkioui75@gmail.com>
2023-03-31 00:18:56 +0200

    init varibale

---
+++ index.js
@@ -0,0 +1,7 @@
+const hole = document.querySelectorAll('.hole')
+const scoreBoard = document.querySelector('.score')
+const heads = document.querySelectorAll('.head');
+
+
+let timeUp = false; // le jeu n'est pas fini = false
+let score = 0;
